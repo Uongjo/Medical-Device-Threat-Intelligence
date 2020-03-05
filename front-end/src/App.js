1bd8db11d8847240d01cf9f4fd5887dd8028a217
Uongjo <Uongjo@utexas.edu>
2020-03-05 16:41:34 -0600

    Add sass and react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './styles/main.scss';
 
 function App() {
   return (

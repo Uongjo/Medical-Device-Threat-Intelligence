76a45b5cd2bdc81f9245304a81869b5a3a079048
Uongjo <Uongjo@utexas.edu>
2020-04-26 19:37:41 -0500

    Add animation for incrementing number

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class Dashboard extends Component {
-  state = {  }
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      totalInventory: 700,
+      animateTotal: 0
+    }
+  }
+
+  componentDidMount() {
+    this.updateCount()
+  }
+
+  updateCount = () => {
+    if(this.state.animateTotal < this.state.totalInventory) {
+      var animateTotal = this.state.animateTotal + 5
+      setInterval(this.updateCount, 1)
+      console.log("Here")
+      this.setState({ animateTotal })
+    }
+  }
+
   render() { 
     return (
       <div id="dashboard">
         <h1>MEDICAL DEVICE THREAT INTELLIGENCE</h1>
+        <h1>{this.state.animateTotal}</h1>
       </div>
     );
   }
